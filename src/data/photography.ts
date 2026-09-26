36500175c251c037803f6d9e8b8403ba05c5d750
mark-myzhao <mingyusysu@gmail.com>
2026-09-25 19:56:04 -0700

    Add externally hosted photography gallery

--- src/data/photography.ts
+++ src/data/photography.ts
@@ -1,3 +1,51 @@
-// Put web-sized images in public/photography. Optionally link your separate site.
-export const photographyWebsite: string = "";
-export const photographs: {src:string; alt:string; title:string; location?:string}[] = [];
+export const photographyWebsite = "https://mysquared.myportfolio.com";
+
+export const photographs: {
+	src: string;
+	alt: string;
+	title: string;
+	href: string;
+}[] = [
+	{
+		title: "City",
+		alt: "City photography",
+		href: "https://mysquared.myportfolio.com/city",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/50ecb208-261a-4729-aee9-11fc924281e7_carw_16x9x1280.jpg?h=c8b48bb238747499716a416c45c0c305",
+	},
+	{
+		title: "Mountain",
+		alt: "Mountain photography",
+		href: "https://mysquared.myportfolio.com/mountain",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/c6b2a7a6-addd-4ce5-bf2a-756a59e9d2da_carw_16x9x1280.jpg?h=b5d9eb63674dc9b08f89b13442e9e938",
+	},
+	{
+		title: "Lake",
+		alt: "Lake photography",
+		href: "https://mysquared.myportfolio.com/lake",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/1ae6233c-ae6a-4202-8c71-0cfd3952d2d3_carw_16x9x1280.jpg?h=9a7f2fe0b526658938c1e1ce2574a4d5",
+	},
+	{
+		title: "The Stars",
+		alt: "Night-sky photography",
+		href: "https://mysquared.myportfolio.com/star",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/b58df80c-923f-43c7-af68-70e2ddb12a0f_carw_16x9x1280.jpg?h=3a2c4f783b5814fee4bf7f251503e079",
+	},
+	{
+		title: "Road",
+		alt: "Road photography",
+		href: "https://mysquared.myportfolio.com/road",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/b94aec99-5e43-424d-8c60-44e860f6d775_carw_16x9x1280.jpg?h=358904fd63bbeb0175d2297b77a3edfc",
+	},
+	{
+		title: "Ocean",
+		alt: "Ocean photography",
+		href: "https://mysquared.myportfolio.com/ocean",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/46fe67d6-a83e-4674-93a3-aac0992459fa_carw_16x9x1280.jpg?h=212770693baf393859291eb63e695091",
+	},
+	{
+		title: "Wildlife",
+		alt: "Wildlife photography",
+		href: "https://mysquared.myportfolio.com/wildlife",
+		src: "https://cdn.myportfolio.com/cf52e289-6ed2-4f39-8707-94b1a0f61415/2505086b-3866-455a-92c5-b146c25466b9_carw_16x9x1280.jpg?h=004cc0a5f48e55b194367b5ebebfb8b7",
+	},
+];
